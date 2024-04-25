944ca044c2e2cc80db9d40a804be3880e4d4480d
MATUMOTO Mitou <kd1336124@st.kobedenshi.ac.jp>
2024-04-25 10:54:14 +0900

    ログイン(仮)

--- login.php
+++ login.php
@@ -3,16 +3,15 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width-device-width, initial-scale-1.0">
-        <title>NAccount</title>
+        <title>ログイン</title>
         <style>
 
         </style>
     </head>
     <body>
-        <!-- ホーム画面に戻るテキストと新規アカウント登録のページであることをユーザーに示すテキスト -->
         <header>
             <a href="index.php">ホーム画面に戻る</a>
-            <h1>新規登録</h1>
+            <h1>ログイン</h1>
         </header>
         <!-- 名前を入力するためのテキストボックス -->
         <label for="textbox">名前:</label>
@@ -25,9 +24,7 @@
         <!-- パスワードを入力するためのテキストボックス -->
         <label for="textbox">パスワード：</label>
         <input type="password" id="pass" name="password"><br>
-
-        <!-- パスワードの確認するためのテキストボックス-->
-        <label for="textbox">再度パスワードの入力：</label>
-        <input type="password" id="checkpass" name="check"><br><br>
+    <br>
+        <a href="NAccount.php">新規登録する</a>
     </body>
 </html>
