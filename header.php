2c8301f7ec75c3d78e2476ab6853e227d4436169
MATUMOTO Mitou <kd1336124@st.kobedenshi.ac.jp>
2024-04-26 14:59:53 +0900

    header.phpの追加

---
+++ header.php
@@ -0,0 +1,26 @@
+ 
+ <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>プログラミング情報共有サイト（仮）</title>
+    <link href="main.css" rel="stylesheet">
+    <script src="index.js" defer></script>
+</head>
+<header>
+        <h1>プログラミング情報共有サイト（仮）</h1>
+
+        <nav>
+            <ul class="nav">
+                <!--ヘッダー ここから-->
+                <li><a href="index.php" class="btn2">ホーム</a></li>
+                <li><a href="Allquestion.php" class="btn4">質問一覧</a></li>
+                <li><a href="Allproject.php" class="btn4">制作物一覧</a></li>
+                <li><a href="profile.php" class="btn4">プロフィール</a></li>
+                <li><a href="login.php" class="btn4">ログイン</a></li>
+                <!--ヘッダー 追加はここから-->
+            </ul>
+        </nav>
+
+    </header>
