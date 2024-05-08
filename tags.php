052de7db7f88543ef8115a3f1339ba519f76cb35
Kenzo Shiga <kd1303741@st.kobedenshi.ac.jp>
2024-05-08 10:55:03 +0900

    tags.phpにaddTagRの関数を作成しました。

--- tags.php
+++ tags.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/dbdata.php';
 
 class Tag extends DbData {
+    // すべてのタグを表示するための関数
     public function showTags() {
         $sql = "select * from tags order by TagID";
         $stmt = $this->query($sql, []);
@@ -10,8 +11,15 @@ class Tag extends DbData {
         return $showTags;
     }
 
+    // 質問とタグのIDを利用して、関連する関数
     public function addTagQ($QuestionID, $TagID) {
         $sql = "INSERT INTO questiontags (QuestionID, TagID) VALUES (?, ?)";
         $stmt = $this->exec($sql, [$QuestionID, $TagID]);
+    }
+
+    // 記事とタグのIDを利用して、関連する関数
+    public function addTagR($RepoID, $TagID) {
+        $sql = "INSERT INTO RepoTags (RepoID, TagID) VALUES (?, ?)";
+        $stmt = $this->exec($sql, [$RepoID, $TagID]);
     }
 }
