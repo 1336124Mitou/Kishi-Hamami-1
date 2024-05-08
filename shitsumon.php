6643da013cd984bf7748fadc50e06aab92cae715
Kenzo Shiga <kd1303741@st.kobedenshi.ac.jp>
2024-05-08 10:32:29 +0900

    shitsumon.phpのaddquestionを更新しました

--- shitsumon.php
+++ shitsumon.php
@@ -10,6 +10,11 @@ class Quest extends DbData
     {
         $sql = "insert into question(D, Tim, Question, UsID) values( ?, ?, ?, ?)"; // 質問内容と投稿時刻を表示する
         $result = $this->exec($sql, [date("Y/m/d"), date("H:i"), $QDet, $UsID]);
+
+        // 最後に入力された質問のIDを取得する
+        $lastQId = $this->pdo->lastInsertId();
+
+        return $lastQId; // 最後に入力された質問のIDを返す
     }
 
     // 質問を表示するするためにすべてのデータを取り出す
