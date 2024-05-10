4c0df394f4d7bb2cd994903b0c710a7cdc41643e
KOKI KISHIMOTO <kd1347722@st.kobedenshi.ac.jp>
2024-05-10 10:57:52 +0900

    kiji

--- repin.php
+++ repin.php
@@ -1,8 +1,8 @@
 <?php
-    if(!isset($kiji)) {
-        require_once __DIR__.'/kiji.php';
-        $kiji = new Report();
-    }
+if (!isset($kiji)) {
+    require_once __DIR__ . '/kiji.php';
+    $kiji = new Report();
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +15,17 @@
     <style>
         .okuru {
             text-align: center;
+        }
+
+        #Title {
+            width: 600px;
+            resize: none;
+        }
+
+        #report {
+            width: 600px;
+            height: 300px;
+            resize: none;
         }
     </style>
 </head>
@@ -31,7 +42,7 @@
 
             <!-- 記事入力 -->
             <h4>記事内容</h4>
-            <textarea id=" report" name="RDet" placeholder="ここに記事を入力してください。" oninput="changeTextColor(this)" required></textarea><br>
+            <textarea id="report" name="RDet" placeholder="ここに記事を入力してください。" oninput="changeTextColor(this)" required></textarea><br>
 
             <input type="submit" value="投稿">
         </form>
