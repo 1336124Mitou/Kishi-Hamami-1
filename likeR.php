234cf68d09bdba3d7d4c5b8badbbf3aab8f69a0e
Kenzo Shiga <kd1303741@st.kobedenshi.ac.jp>
2024-05-15 11:36:55 +0900

    kishi.sqlとlikeR更新

--- likeR.php
+++ likeR.php
@@ -6,13 +6,13 @@ class LikeRepo extends Dbdata
 {
     public function addLikeR($RepoID)
     {
-        $sql = "UPDATE Reply SET LNum = LNum + 1 WHERE RepID = ?";
+        $sql = "UPDATE Report SET LNum = LNum + 1 WHERE RepoID = ?";
         $result = $this->exec($sql, [$RepoID]);
     }
 
     public function showLikeR($RepoID)
     {
-        $sql = "SELECT LNum FROM Reply Where RepID = ?";
+        $sql = "SELECT LNum FROM Report Where RepoID = ?";
         $stmt = $this->query($sql, [$RepoID]);
         $showLike = $stmt->fetch();
         return $showLike;
