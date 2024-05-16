6a0d9022d66c9f353b0914ab9a637b213fab4d31
KOKI KISHIMOTO <kd1347722@st.kobedenshi.ac.jp>
2024-05-16 12:03:40 +0900

    エラーのバカヤロー

--- Kishi.sql
+++ Kishi.sql
@@ -153,7 +153,7 @@ DROP TABLE if EXISTS Project;
 
 -- テーブルProjectの作成
 CREATE TABLE Project (
-    ProID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    ProID INT PRIMARY KEY AUTO_INCREMENT,
     ProName VARCHAR(255) NOT NULL,
     ProjFile LONGBLOB NOT NULL,
     Proexample VARCHAR(255) NOT NULL
@@ -190,19 +190,32 @@ CREATE TABLE Likes (
     UNIQUE KEY unique_repo_like (UsID, RepoID)
 );
 
--- ユーザのデータが他のデータとちゃんと関連するためのテーブル
--- tableがあるなら削除
-DROP TABLE if EXISTS USlink;
+-- ユーザと関連するデータのリンクを保持するテーブル
+-- テーブルがあるなら削除
+DROP TABLE IF EXISTS USlink;
 
 -- USlinkのテーブルを作成する
 CREATE TABLE USlink (
     UsID VARCHAR(50) NOT NULL,
     ProID INT DEFAULT NULL,
-    RepID INT DEFAULT NULL,
     RepoID INT DEFAULT NULL,
-    FOREIGN KEY (ProID) REFERENCES Project(ProID),
+    RepID INT DEFAULT NULL,
     FOREIGN KEY (UsID) REFERENCES Usr(UsID),
-    FOREIGN KEY (RepID) REFERENCES Reply(RepID),
+    FOREIGN KEY (ProID) REFERENCES Project(ProID),
     FOREIGN KEY (RepoID) REFERENCES Report(RepoID),
-    PRIMARY KEY (ProID, RepoID, UsID, RepID)
+    FOREIGN KEY (RepID) REFERENCES Reply(RepID),
+    PRIMARY KEY (UsID, ProID, RepoID, RepID)
+);
+
+-- ユーザーと関連する記事データのリンクを保持するテーブル
+-- テーブルがあるから削除
+DROP TABLE IF EXISTS URlink;
+
+-- URlinkのテーブルを作成する
+CREATE TABLE URlink (   
+    UsID VARCHAR(50) NOT NULL,
+    RepoID INT DEFAULT NULL,
+    FOREIGN KEY (UsID) REFERENCES Usr(UsID),
+    FOREIGN KEY (RepoID) REFERENCES Report(RepoID),
+    PRIMARY KEY (UsID, RepoID)
 );
