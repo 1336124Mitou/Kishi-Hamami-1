e06a6dcf620bfb28070a356a9a2d8e211ca82ee5
Nobuhiro Hayashi <kd1334632@st.kobedenshi.ac.jp>
2024-05-16 14:57:24 +0900

    自己紹介以外完成

--- profile.php
+++ profile.php
@@ -24,10 +24,12 @@
             margin-left: 120px;
             position: relative;
         }
+
         .profile-header {
             display: flex;
             align-items: center;
         }
+
         .profile-img {
             width: 80px;
             height: 80px;
@@ -35,41 +37,49 @@
             object-fit: cover;
             margin-right: 20px;
         }
+
         .profile-name {
             font-size: 24px;
             font-weight: bold;
         }
+
         .profile-emailaddress {
             font-size: 14px;
             color: #777;
         }
+
         .profile-bio {
             font-size: 14px;
             color: #555;
             margin: 10px 0;
         }
+
         .profile-stats {
             display: flex;
             justify-content: space-around;
             align-items: flex-start;
             margin: 20px 0;
         }
+
         .profile-stat {
             display: flex;
             align-items: center;
             width: 100%;
             margin-bottom: 10px;
         }
+
         .profile-stat label {
             font-size: 12px;
             color: #777;
             margin-left: 100px;
         }
+
         .profile-stat hr {
             flex-grow: 1;
             border: none;
             border-top: 1px solid #ddd;
         }
+
         .edit-profile-button {
             background-color: #007bff;
             color: white;
@@ -82,6 +92,7 @@
             top: 20px;
             right: 20px;
         }
+
         .edit-profile-button:hover {
             background-color: #0056b3;
         }
@@ -92,24 +103,23 @@
     <?php require_once __DIR__ . '/header.php'; ?>
 
     <div class="profile-container">
-    <button class="edit-profile-button">プロフィールを編集</button>
-    <div class="profile-header">
-        <img src="1676155437876-5NNUYKTjTE.png" alt="プロフィール画像" class="profile-img">
-        <div>
-            <div class="profile-name">岸本 昂己</div>
-            <div class="profile-emailaddress">kd1347722@st.kobedenshi.ac.jp</div>
+        <button class="edit-profile-button">プロフィールを編集</button>
+        <div class="profile-header">
+            <img src="1676155437876-5NNUYKTjTE.png" alt="プロフィール画像" class="profile-img">
+            <div>
+                <div class="profile-name"><?= $_SESSION['userName'] ?></div>
+                <div class="profile-emailaddress"><?= $_SESSION['userId'] ?></div>
+            </div>
         </div>
-    </div>
-    <div class="profile-stats">
-        <div class="profile-stat">
-            <label>プロフィール</label>
-            <hr>
+        <div class="profile-stats">
+            <div class="profile-stat">
+                <label>プロフィール</label>
+                <hr>
+            </div>
         </div>
-    </div>
-    <div class="profile-bio">なんでも頑張ります！</div>
+        <div class="profile-bio">なんでも頑張ります！</div>
     </div>
 
 </body>
 
 </html>
-    
