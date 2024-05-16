d1cdc432722abeff6096debaf7b7eb257398b1b7
Kenzo Shiga <kd1303741@st.kobedenshi.ac.jp>
2024-05-16 14:11:31 +0900

    user.php更新

--- user.php
+++ user.php
@@ -23,8 +23,19 @@ class RU extends Dbdata
 
 class User extends Dbdata
 {
-    public function NewUser($USID, $Name, $password, $ProInfo, $ProPic)
+    public function NewUser($USID, $Name, $password, $passCheck, $ProInfo, $ProPic)
     {
-        
+        $sql = "SELECT * FROM usr WHERE UsID = ?";
+        $stmt = $this->query($sql, [$USID]);
+        $result = $stmt->fetch();
+        if ($result) {
+            return 'この'. $USID . 'は既に登録されています。';
+        }
+        if ($password == $passCheck) {
+            $sql = "INSERT INTO usr(UsID, UsName, Passw, Prof, ProfPic) values(?, ?, ?, ?, ?)";
+            $result = $this->exec($sql, [$USID, $Name, $password, $ProInfo, $ProPic]);
+        } else {
+            return 'パスワードの入力が間違っています';
+        }
     }
 }
