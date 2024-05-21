9de543470d7a075aebe3a7897312e99024378dec
Kenzo Shiga <kd1303741@st.kobedenshi.ac.jp>
2024-05-21 10:55:24 +0900

    user.phpとallquestionの修正

--- user.php
+++ user.php
@@ -2,8 +2,47 @@
 
 require_once __DIR__ . '/dbdata.php';
 
+class UR extends Dbdata
+{
+    public function insertURlink($UsID, $RepoID)
+    {
+        // SQL query to insert data into USlink table with UsID and RepoID
+        $sql = "INSERT INTO URlink (UsID, RepoID) VALUES (?, ?)";
+        // Execute the SQL query with provided UsID and RepoID
+        $result = $this->exec($sql, [$UsID, $RepoID]);
+    }
+}
+// ユーザーのデータと制作物のデータを関連させる
+class UP extends Dbdata
+{
+    public function insertUPlink($UsID, $ProID)
+    {
+        $sql = "INSERT INTO UPlink (UsID, ProID) VALUES (?, ?)";
+        $result = $this->exec($sql, [$UsID, $ProID]);
+    }
+}
+// ユーザーのデータと回答のデータを関連させる
+class URe extends Dbdata
+{
+    public function insertURelink($UsID, $RepID)
+    {
+        $sql = "INSERT INTO URelink (UsID, RepID) VALUES (?, ?)";
+        $result = $this->exec($sql, [$UsID, $RepID]);
+    }
+}
+// ユーザーのデータと質問のデータを関連させる
+class UQ extends Dbdata
+{
+    public function insertUQlink($UsID, $QuestionID)
+    {
+        $sql = "INSERT INTO UQlink (UsID, QuestionID) VALUES (?, ?)";
+        $result = $this->exec($sql, [$UsID, $QuestionID]);
+    }
+}
+
 class User extends Dbdata
 {
+    
     // 新しいユーザーを登録するメソッド
     public function newUser($UsID, $Name, $password, $passCheck, $ProInfo, $ProPic)
     {
